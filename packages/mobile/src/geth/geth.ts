8fe81b36e57daff2f14998d4dc8a05e506a88309
annakaz <annakaz@users.noreply.github.com>
2019-10-29 12:23:19 -0700

    Clear geth cache, rework sagas

--- packages/mobile/src/geth/geth.ts
+++ packages/mobile/src/geth/geth.ts
@@ -153,6 +153,10 @@ async function initGeth() {
   }
 }
 
+export async function clearGethCache() {
+  gethInstance = null
+}
+
 export async function getGeth(): Promise<typeof gethInstance> {
   Logger.debug('Geth@getGeth', 'Getting Geth Instance')
   if (!gethInstance) {
