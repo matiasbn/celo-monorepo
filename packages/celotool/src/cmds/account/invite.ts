d2b2e6f1d2288fd308d6aaad5cdedc7d1f7272fa
Connor McEwen <connor.mcewen@gmail.com>
2019-11-09 14:30:57 +0000

    New invite script

--- packages/celotool/src/cmds/account/invite.ts
+++ packages/celotool/src/cmds/account/invite.ts
@@ -1,5 +1,6 @@
 /* tslint:disable no-console */
 import { newKit } from '@celo/contractkit'
+import { StableTokenWrapper } from '@celo/contractkit/lib/wrappers/StableTokenWrapper'
 import { BigNumber } from 'bignumber.js'
 import { portForwardAnd } from 'src/lib/port_forward'
 import { Argv } from 'yargs'
@@ -15,22 +16,26 @@ interface InviteArgv extends AccountArgv {
 }
 
 export const builder = (yargs: Argv) => {
-  return yargs
-    .option('phone', {
-      type: 'string',
-      description: 'Phone number to send invite code,',
-      demand: 'Please specify phone number to send invite code',
-    })
-    .option('fast', {
-      type: 'boolean',
-      default: false,
-      description: "Don't download artifacts, use this for repeated invocations",
-      demand: 'Please specify phone number to send invite code',
-    })
+  return yargs.option('phone', {
+    type: 'string',
+    description: 'Phone number to send invite code,',
+    demand: 'Please specify phone number to send invite code',
+  })
+}
+
+export async function convertToContractDecimals(
+  value: number | BigNumber,
+  contract: StableTokenWrapper
+) {
+  const decimals = new BigNumber(await contract.decimals())
+  const one = new BigNumber(10).pow(decimals.toNumber())
+  return one.times(value)
 }
 
 export const handler = async (argv: InviteArgv) => {
-  console.log(`Sending invitation code to ${argv.phone}`)
+  const phone = argv.phone
+
+  console.log(`Sending invitation code to ${phone}`)
   const cb = async () => {
     const kit = newKit('http://localhost:8545')
     const account = (await kit.web3.eth.getAccounts())[0]
@@ -48,26 +53,46 @@ export const handler = async (argv: InviteArgv) => {
     const privateKeyHex = temporaryWalletAccount.privateKey.substring(2)
     const inviteCode = Buffer.from(privateKeyHex, 'hex').toString('base64')
 
-    const [goldToken, stableToken, attestations] = await Promise.all([
+    const [goldToken, stableToken, attestations, escrow] = await Promise.all([
       kit.contracts.getGoldToken(),
       kit.contracts.getStableToken(),
       kit.contracts.getAttestations(),
+      kit._web3Contracts.getEscrow(),
     ])
     const verificationFee = new BigNumber(
       await attestations.attestationRequestFees(stableToken.address)
     )
     const goldAmount = verificationGasAmount.times(gasPrice).toString()
-    const stableTokenAmount = verificationFee.times(10).toString()
+    const stableTokenInviteAmount = verificationFee.times(10).toString()
+    const stableTokenEscrowAmount = (await convertToContractDecimals(5, stableToken)).toString()
 
-    console.log(`Transferring ${goldAmount} Gold and ${stableTokenAmount} StableToken`)
+    const phoneHash: string = kit.web3.utils.soliditySha3({
+      type: 'string',
+      value: phone,
+    })
+
+    await stableToken.approve(escrow._address, stableTokenEscrowAmount).sendAndWaitForReceipt()
+    const expirySeconds = 60 * 60 * 24 * 5 // 5 days
+
+    console.log(
+      `Transferring ${goldAmount} Gold, ${stableTokenInviteAmount} StableToken, and escrowing ${stableTokenEscrowAmount}`
+    )
     await Promise.all([
       goldToken.transfer(temporaryAddress, goldAmount).sendAndWaitForReceipt(),
-      stableToken.transfer(temporaryAddress, stableTokenAmount).sendAndWaitForReceipt(),
+      stableToken.transfer(temporaryAddress, stableTokenInviteAmount).sendAndWaitForReceipt(),
+      escrow.methods
+        .transfer(
+          phoneHash,
+          stableToken.address,
+          stableTokenEscrowAmount,
+          expirySeconds,
+          temporaryAddress,
+          0
+        )
+        .call(),
     ])
     console.log(`Temp address: ${temporaryAddress}`)
     console.log(`Invite code: ${inviteCode}`)
-
-    return [temporaryAddress, inviteCode]
   }
   try {
     await portForwardAnd(argv.celoEnv, cb)
